9278657364ddc25cedd29a9d72869defab80cde5
TaiwanKHJHGTea <42378738+TaiwanKHJHGTea@users.noreply.github.com>
2018-08-14 21:29:10 +0800

    Update Weather.java
    繁體中文化

--- src/main/java/uk/co/harieo/seasons/models/Weather.java
+++ src/main/java/uk/co/harieo/seasons/models/Weather.java
@@ -8,55 +8,55 @@ import uk.co.harieo.seasons.models.effect.Effect;
 
 public enum Weather {
 
-	BEAUTIFUL("Beautiful",
-			ChatColor.GREEN + "The sun in shining, the grass is green and the weather is " + ChatColor.YELLOW
-					+ "Beautiful",
+	BEAUTIFUL("美極了",
+			ChatColor.GREEN + "閃耀的太陽, 草是綠油油der 且這天氣... " + ChatColor.YELLOW
+					+ "真是美好",
 			false, false,
 			Arrays.asList(Season.SPRING, Season.SUMMER)),
-	BREEZY("Breezy",
-			ChatColor.GRAY + "A cool breeze touches your skin, it's going to be " + ChatColor.GREEN + "Breezy",
+	BREEZY("微風輕拂",
+			ChatColor.GRAY + "涼爽的微風輕拂你的身旁,這將會變得 " + ChatColor.GREEN + "更加愉悅(吸大麻?",
 			false, false,
 			Arrays.asList(Season.SPRING, Season.AUTUMN)),
-	CHILLY("Chilly",
-			ChatColor.BLUE + "You shiver as frost glistens around you, it's very " + ChatColor.DARK_BLUE + "Chilly "
-					+ ChatColor.BLUE + "today",
+	CHILLY("變冷了",
+			ChatColor.BLUE + "當霜凍霜在你周圍閃閃發光時，你將會顫抖, 這將會變得 " + ChatColor.DARK_BLUE + "寒冷... "
+					+ ChatColor.BLUE + "就是今天",
 			false, false,
 			Collections.singletonList(Season.SPRING)),
-	RAINY("Rainy",
-			ChatColor.BLUE + "A loud rain falls and wets the ground, it's " + ChatColor.DARK_BLUE + "Rainy",
+	RAINY("下雨了",
+			ChatColor.BLUE + "周圍的地變得又濕又滑, 這是... " + ChatColor.DARK_BLUE + "雨",
 			false, true,
 			Arrays.asList(Season.SPRING, Season.AUTUMN, Season.WINTER)),
-	SCORCHING("Scorching",
-			ChatColor.YELLOW + "The sun burns your skin and the ground hurts to touch, it's " + ChatColor.GOLD
-					+ "Scorching",
+	SCORCHING("酷熱",
+			ChatColor.YELLOW + "熾熱的太陽灼燒了妳的皮膚，地表開始加熱, 這是... " + ChatColor.GOLD
+					+ "酷熱",
 			false, false,
 			Collections.singletonList(Season.SUMMER)),
-	HOT("Hot",
-			ChatColor.YELLOW + "It's going to be very " + ChatColor.GOLD + "Hot " + ChatColor.YELLOW
-					+ "today",
+	HOT("好熱",
+			ChatColor.YELLOW + "今天變得越來越 " + ChatColor.GOLD + "熱 " + ChatColor.YELLOW
+					+ "",
 			false, false,
 			Collections.singletonList(Season.SUMMER)),
-	WARM("Warm",
-			ChatColor.YELLOW + "A soothing warmth hugs you as you move, it's a " + ChatColor.GOLD + "Warm "
-					+ ChatColor.YELLOW + "day",
+	WARM("好溫暖",
+			ChatColor.YELLOW + "當你移動時，一種舒緩的溫暖擁抱著你, 好..好 " + ChatColor.GOLD + "溫暖 "
+					+ ChatColor.YELLOW + "的一天",
 			false, false,
 			Collections.singletonList(Season.SUMMER)),
-	COLD("Cold",
-			ChatColor.BLUE + "The water is so very " + ChatColor.DARK_BLUE + "Cold " + ChatColor.BLUE + "today",
+	COLD("好冷",
+			ChatColor.BLUE + "水已經非常的 " + ChatColor.DARK_BLUE + "冷了 " + ChatColor.BLUE + "",
 			false, false,
 			Arrays.asList(Season.AUTUMN, Season.WINTER)),
-	STORMY("Stormy",
-			ChatColor.RED + "A great " + ChatColor.DARK_RED + "Storm " + ChatColor.RED
-					+ "brews, the Gods in this place are angry... Brace yourself!",
+	STORMY("暴風雨來襲",
+			ChatColor.RED + "強烈的 " + ChatColor.DARK_RED + "暴風雨 " + ChatColor.RED
+					+ "產生了, 神明在這裡生氣了... 自己要多保重!",
 			true, true,
 			Collections.singletonList(Season.AUTUMN)),
-	FREEZING("Freezing",
-			ChatColor.BLUE + "The water freezes with a sheet of ice and you feel a great cold, it's "
-					+ ChatColor.DARK_BLUE + "Freezing",
+	FREEZING("雪虐風饕虐",
+			ChatColor.BLUE + "水被一塊一塊地被凍結成冰，你感覺很冷, 這真是.. "
+					+ ChatColor.DARK_BLUE + "冰冷至極",
 			true, false,
 			Collections.singletonList(Season.WINTER)),
-	SNOWY("Snowy",
-			ChatColor.GRAY + "A great white blanket covers the world, it's " + ChatColor.WHITE + "Snowy",
+	SNOWY("靄靄白雪",
+			ChatColor.GRAY + "一條白色的大毯覆蓋了整個世界, 這是.. " + ChatColor.WHITE + "雪",
 			false, true,
 			Collections.singletonList(Season.WINTER)),
 	NIGHT("Calm", // Night is a weather with no effect, to give people a break
